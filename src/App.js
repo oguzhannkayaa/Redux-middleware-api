17ffdaae34233491450ebc496284feca40e8a75c
oguzhan10 <kayaoguzhan@windowslive.com>
2023-01-20 09:20:05 +0300

    finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import {connect} from 'react-redux';
+import {getCountries} from './services/actions';
 
-function App() {
+function App(props) {
+
+  useEffect(() => {
+    props.getCountries();
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {props.isLoading ? <p>Loading</p> : props.countries.map(country => {
+        return (
+          <div key={country.name}>
+            <h1>{country.name}</h1>
+          </div>
+        )
+      }) }
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    countries: state.countries,
+    isLoading: state.isLoading
+  }
+}
+
+
+export default connect(mapStateToProps,{getCountries})(App);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { applyMiddleware, createStore } from "redux";
+import { Provider } from "react-redux";
+import { reducer } from "./services/reducers";
+import thunk from "redux-thunk";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = createStore(reducer, applyMiddleware(thunk));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
